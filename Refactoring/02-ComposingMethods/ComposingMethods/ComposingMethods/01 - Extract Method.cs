b31aaff03dac9024b2c74bd7f14d6138ae732904
vintharas <jaime.gg83@gmail.com>
2012-12-04 20:53:29 +0100

    Refactoring: Created solution for 'Composing Methods' refactorings
       Created a new solution for grouping the 'Composing Methods'
       refactoring examples.

---
+++ Refactoring/02-ComposingMethods/ComposingMethods/ComposingMethods/01 - Extract Method.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposingMethods
+{
+
+    /* Extract Method
+     * 
+     * Problem: You have a code fragment that can be grouped together.
+     * 
+     * Refactoring: Turn the fragment into a method whose name explains the purpose of the method
+     * 
+     * 
+     * 
+     */
+
+    public class Barbarian
+    {
+
+    }
+
+    public class BarbarianRefactored
+    {
+        
+    }
+
+
+}
---
+++ Refactoring/02-ComposingMethods/README.markdown
@@ -0,0 +1,13 @@
+# Composing Methods
+
+Refactorings for managing methods.
+
+   1.  Extract Method
+   2.  Inline Method
+   3.  Inline Temp
+   4.  Replace Temp with Query
+   5.  Introduce Explaining Variable
+   6.  Split Temporary Variable
+   7.  Remove Assignments to Parameters
+   8.  Replace Method with Method Object
+   9.  Substitute Algorithm
