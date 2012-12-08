41de1322c51f8808a41181be8629c57460f7c368
vintharas <jaime.gg83@gmail.com>
2012-12-08 07:31:11 +0100

    Refactoring: Added example for Extract Method refactoring
       Added an example and unit test for the Extract Method refactoring

--- Refactoring/02-ComposingMethods/ComposingMethods/ComposingMethods.UnitTests/01 - Extract Method Tests.cs
+++ Refactoring/02-ComposingMethods/ComposingMethods/ComposingMethods.UnitTests/01 - Extract Method Tests.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ComposingMethods.UnitTests
 {
-    public class Class1
+    [TestFixture]
+    public class BarbarianTests
     {
+        [Test]
+        public void Attack_WhenGivenAMonster_ShouldHitMonsterWithHisWeapon()
+        {
+            // Arrange
+            var barbarian = new Barbarian {Name = "Conan", Weapon = new Weapon {Name = "Sword", Damage = 1.5}, HitPoints = 150};
+            var monster = new Monster {Name = "Goblin", HitPoints = 10};
+            // Act
+            string description = barbarian.Attack(monster);
+            // Assert
+            Assert.That(description, Is.StringContaining("Conan swings his Sword and hits the Goblin in strength. Goblin loses"));
+        }
+
     }
 }
--- Refactoring/02-ComposingMethods/ComposingMethods/ComposingMethods/01 - Extract Method.cs
+++ Refactoring/02-ComposingMethods/ComposingMethods/ComposingMethods/01 - Extract Method.cs
@@ -17,15 +17,93 @@ namespace ComposingMethods
      * 
      */
 
-    public class Barbarian
+    public class Character
     {
-
+        public double HitPoints { get; set; }
     }
 
-    public class BarbarianRefactored
+    public class Die10
     {
-        
+        private Random random = new Random();
+
+        public int Roll()
+        {
+            return random.Next(1, 10);
+        }
     }
+
+    public class Barbarian : Character
+    {
+        public string Name { get; set; }
+        public Weapon Weapon { get; set; }
+
+        public string Describe()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hither came Conan, the Cimmerian, black-haired, sullen-eyed, sword in hand, a thief, a reaver, a slayer, with gigantic melancholies and gigantic mirth, to tread the jeweled thrones of the Earth under his sandalled feet.");
+            return sb.ToString();
+        }
+
+        public string Attack(Monster monster)
+        {
+            Die10 die = new Die10();
+            double damageInflicted = die.Roll() + die.Roll() + Weapon.Damage;
+            monster.HitPoints -= damageInflicted;
+            return string.Format("{0} swings his {1} and hits the {2} in strength. Goblin loses {3} HP.", Name, Weapon, monster, damageInflicted);
+        }
+    }
+
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public double Damage { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Monster : Character
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+    // Refactored Version
+    // public class Barbarian : Character
+    // {
+    //     public string Name { get; set; }
+    //     public Weapon Weapon { get; set; }
+
+    //     public string Describe()
+    //     {
+    //         StringBuilder sb = new StringBuilder();
+    //         sb.Append("Hither came Conan, the Cimmerian, black-haired, sullen-eyed, sword in hand, a thief, a reaver, a slayer, with gigantic melancholies and gigantic mirth, to tread the jeweled thrones of the Earth under his sandalled feet.");
+    //         return sb.ToString();
+    //     }
+
+    //     public string Attack(Monster monster)
+    //     {
+    //         double damageInflicted = CalculateDamageInflicted();
+    //         monster.HitPoints -= damageInflicted;
+    //         return string.Format("{0} swings his {1} and hits the {2} in strength. Goblin loses {3} HP.", Name, Weapon, monster, damageInflicted);
+    //     }
+
+    //     /// Extracted method for calculating inflicted damage
+    //     private double CalculateDamageInflicted()
+    //     {
+    //         Die10 die = new Die10();
+    //         double damageInflicted = die.Roll() + die.Roll() + Weapon.Damage;
+    //         return damageInflicted;
+    //     }
+    //}
+    
 
 
 }
